97d53d27423596c8a310e8caab6739be15334202
âsusuzimo <1432625686@qq.com>
2019-11-20 08:35:21 +0800

    首页登录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+import {postKeyValueRequest} from './utils/api'
+import {postRequest} from './utils/api'
+import {putRequest} from './utils/api'
+import {getRequest} from './utils/api'
+import {deleteRequest} from './utils/api'
+
+Vue.prototype.postKeyValueRequest=postKeyValueRequest
+Vue.prototype.postRequest=postRequest
+Vue.prototype.putRequest=putRequest
+Vue.prototype.getRequest=getRequest
+Vue.prototype.deleteRequest=deleteRequest
+
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
